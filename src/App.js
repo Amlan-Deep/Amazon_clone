2b622d9931aa8ef7d009fc6da0a3327fd2480a65
Amlan Deep Mohapatra <amlandeep09012003@gmail.com>
2023-06-01 10:29:45 +0530

    commit 1st june

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+
 function App() {
-return (
-  <div className="app">
-    <Router>
-      <Routes>
-      <Route path="/" element={<Header/>}/>
-      <Route exact path="/" element={<Home/>} />
-      
-      
-      </Routes>
-    </Router>
-  </div>
-);
+  return (
+    <div className="app">
+      <Router>
+        <Header /> \
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          {/* Other routes and components */}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
+
 export default App;
